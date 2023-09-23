88aa992798056ce8a2d59036b1efde5cf34f03bc
ch720187 <144292139+ch720187@users.noreply.github.com>
2023-09-23 13:09:16 -0400

    Add files via upload

---
+++ HW2/Project 2/Original_CSVtoSQL_tables/mort 3.sql
@@ -0,0 +1,33 @@
+CREATE TABLE mytable(
+   Year               INTEGER  NOT NULL PRIMARY KEY 
+  ,under_5_Mortality  VARCHAR(4) NOT NULL
+  ,Infant_Mortality   VARCHAR(4) NOT NULL
+  ,Neonatal_Mortality VARCHAR(4) NOT NULL
+);
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1990,'93.4','64.8','36.8');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1991,'92.1','63.9','36.3');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1992,'90.9','63.1','35.9');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1993,'89.7','62.3','35.4');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1994,'88.7','61.4','??');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1995,'87.3','60.5','34.4');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1996,'85.6','59.4','33.7');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1997,'??','58.2','33.1');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1998,'82.1','56.9','32.3');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (1999,'79.9','55.4','31.5');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2000,'77.5','53.9','30.7');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2001,'74.8','52.1','29.8');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2002,'72','??','28.9');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2003,'69.2','48.6','28');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2004,'66.7','46.9','??');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2005,'??','45.1','26.1');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2006,'61.1','43.4','25.3');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2007,'58.5','??','24.4');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2008,'56.2','40.3','23.6');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2009,'53.7','38.8','22.9');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2010,'??','37.4','22.2');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2011,'49.3','36','21.5');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2012,'47.3','34.7','20.8');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2013,'45.5','33.6','20.2');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2014,'43.7','??','19.6');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2015,'42.2','31.4','19.1');
+INSERT INTO mytable(Year,under_5_Mortality,Infant_Mortality,Neonatal_Mortality) VALUES (2016,'40.8','30.5','18.6');
